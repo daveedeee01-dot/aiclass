419a7f319a2e607d34c8944592d5fc5cc6036f03
daveedeee01-dot <daveedeee01@gmail.com>
2025-08-14 01:02:54 -0700

    p prime

---
+++ blueprint.py
@@ -0,0 +1,8 @@
+class quiz:
+    def __init__(self,questions):
+          self.questions=questions
+          self.score=0
+    def display_questions(self):
+        for questions in self.questions
+        
+        
